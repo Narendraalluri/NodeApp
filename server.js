9bf9ca8a96436a1d044eab63a8f787853c509bfd
nalluri <narenda.alluri@one.verizon.com>
2015-01-05 02:08:59 -0600

    Run local using node serve.js local

--- server.js
+++ server.js
@@ -3,53 +3,85 @@ var express = require('express'),
 
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var multer  = require('multer')
-app.use(multer({ dest: './uploads/'}))
+var multer = require('multer')
+app.use(multer({
+    dest: './uploads/'
+}))
 
 var html_dir = __dirname + '/static';
 app.use(express.static(html_dir));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(express.cookieParser());
-app.use(express.session({secret: '1234567890QWERTY'}));
+app.use(express.session({
+    secret: '1234567890QWERTY'
+}));
 
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://root:FbVQsj0Zx2LW@localhost/admin');
 
-//mongoose.connect('mongodb://localhost/TimeSheetDB');
+
+if(process.argv[2] == 'local')
+    mongoose.connect('mongodb://localhost/TimeSheetDB');
+else
+    mongoose.connect('mongodb://root:FbVQsj0Zx2LW@localhost/admin');
+
+//
 
 
 var fs = require('fs');
 var Sheets = new mongoose.Schema({
-    project     : String
-  , sunday      : Number
-  , monday      : Number
-  , tuesday      : Number
-  , wednesday      : Number
-  , thursday      : Number
-  , friday      : Number
-  , saturday      : Number
-  , total      : Number
-  , status      : Boolean
+    project: String,
+    sunday: Number,
+    monday: Number,
+    tuesday: Number,
+    wednesday: Number,
+    thursday: Number,
+    friday: Number,
+    saturday: Number,
+    total: Number,
+    status: Boolean
 });
 var File = new mongoose.Schema({
-    id     : String
-  , name      : String
- 
+    id: String,
+    name: String
+
 });
 //var timeSheet = mongoose.model('TimeSheet', { clientApproved: String , userId : String,userName: String , startDate : String, endDate : String, monday : String, tuesday : String, wednesday : String, thursday : String, friday : String, saturday : String, sunday : String,  content : Buffer, originalName: String, status : Number, createDate : Date });
-var timeSheet = mongoose.model('TimeSheet', { clientApproved: String , userId : String,userName: String, name: String , startDate : String, endDate : String, sheets : [Sheets], total : Number, comments: String, uploadedFiles: [File]});
+var timeSheet = mongoose.model('TimeSheet', {
+    clientApproved: String,
+    userId: String,
+    userName: String,
+    name: String,
+    startDate: String,
+    endDate: String,
+    sheets: [Sheets],
+    total: Number,
+    comments: String,
+    uploadedFiles: [File]
+});
 
-var FileContent = mongoose.model('FileContent', {  content : Buffer });
+var FileContent = mongoose.model('FileContent', {
+    content: Buffer
+});
 
-var user = mongoose.model('User', { firstName: String , lastName : String, email : String, password : String, role : String});
+var user = mongoose.model('User', {
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    role: String
+});
 
 app.get('/viewUserTimesheets', function(req, res) {
 
     userId = req.query.userId
-    
-    timeSheet.find({"userId":userId},'_id startDate endDate total clientApproved', function(err, data) {
+
+    timeSheet.find({
+        "userId": userId
+    }, '_id startDate endDate total clientApproved', function(err, data) {
 
         res.json(data);
 
@@ -59,8 +91,8 @@ app.get('/viewUserTimesheets', function(req, res) {
 app.get('/viewAllTimesheets', function(req, res) {
 
     userId = req.query.userId
-    
-    timeSheet.find({},'_id userName startDate endDate total clientApproved', function(err, data) {
+
+    timeSheet.find({}, '_id userName startDate endDate total clientApproved', function(err, data) {
 
         res.json(data);
 
@@ -69,18 +101,20 @@ app.get('/viewAllTimesheets', function(req, res) {
 
 
 app.get('/getTimesheet', function(req, res) {
-    
-    timeSheet.find({'_id': req.query.id}, function(err, data) {
-console.log("In View user TimeSheet "+req.query.id+"  "  +data)
+
+    timeSheet.find({
+        '_id': req.query.id
+    }, function(err, data) {
+        console.log("In View user TimeSheet " + req.query.id + "  " + data)
         res.json(data);
 
     });
 });
 
 app.get('/viewTimesheets', function(req, res) {
-console.log("In View TimeSheet")
+    console.log("In View TimeSheet")
     userId = req.session.loggedInId
-    if(req.query != null && req.query.id != null){
+    if (req.query != null && req.query.id != null) {
         userId = req.query.id
         console.log(req.query.id)
     }
@@ -89,7 +123,12 @@ console.log("In View TimeSheet")
 
     date3Months = new Date();
     date3Months.setMonth(date3Months.getMonth() - 3)
-    timeSheet.find({'name': userId, 'createDate' : {  '$gt': date3Months } },'clientApproved name originalName startDate endDate monday tuesday wednesday thursday friday saturday sunday _id status', function(err, data) {
+    timeSheet.find({
+        'name': userId,
+        'createDate': {
+            '$gt': date3Months
+        }
+    }, 'clientApproved name originalName startDate endDate monday tuesday wednesday thursday friday saturday sunday _id status', function(err, data) {
 
         res.json(data);
 
@@ -102,24 +141,35 @@ app.get('/deleteUser', function(req, res) {
 
     user.findByIdAndRemove(req.query.id, function(err, data) {
 
-         res.json({"status":"success"});
+        res.json({
+            "status": "success"
+        });
 
     });
 });
 
 app.post('/updateUser', function(req, res) {
 
-    console.log("Update User "+req.body.id)
+    console.log("Update User " + req.body.id)
 
-        user.findByIdAndUpdate(req.body.id, { $set: {  firstName: req.body.firstName,lastName: req.body.lastName,email: req.body.email,role: req.body.role }}, function (err, tank) {
-          //res.sendfile(html_dir + '/Users.html');
-              console.log("Update User "+err)
+    user.findByIdAndUpdate(req.body.id, {
+        $set: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            role: req.body.role
+        }
+    }, function(err, tank) {
+        //res.sendfile(html_dir + '/Users.html');
+        console.log("Update User " + err)
 
-            res.json({"status":"success"});
-
-         
-
+        res.json({
+            "status": "success"
         });
+
+
+
+    });
 
 });
 
@@ -163,32 +213,56 @@ app.get('/deleteTimeSheet', function(req, res) {
 
 app.post('/updateTimesheet', function(req, res) {
 
-    
+
     var ts = new timeSheet;
-    try{
+    try {
         var tmp_path = req.files.timesheet.path;
-    fs.readFile(tmp_path, function(err, data){
-        console.log("before")
-        timeSheet.findByIdAndUpdate(req.body.id, { $set: { clientApproved: req.body.clientApproved, originalName: req.files.timesheet.originalname, content: data, sunday: req.body.sunday,monday: req.body.monday,tuesday: req.body.tuesday,wednesday: req.body.wednesday,thursday: req.body.thursday,friday: req.body.friday,saturday: req.body.saturday }}, function (err, tank) {
-console.log("After")
-          fs.unlink(tmp_path, function(err) {
-                res.sendfile(html_dir + '/viewTimeSheets.html');
-                //res.send('File uploaded to: ' + target_path + ' - ' + req.files.timesheet.size + ' bytes');
+        fs.readFile(tmp_path, function(err, data) {
+            console.log("before")
+            timeSheet.findByIdAndUpdate(req.body.id, {
+                $set: {
+                    clientApproved: req.body.clientApproved,
+                    originalName: req.files.timesheet.originalname,
+                    content: data,
+                    sunday: req.body.sunday,
+                    monday: req.body.monday,
+                    tuesday: req.body.tuesday,
+                    wednesday: req.body.wednesday,
+                    thursday: req.body.thursday,
+                    friday: req.body.friday,
+                    saturday: req.body.saturday
+                }
+            }, function(err, tank) {
+                console.log("After")
+                fs.unlink(tmp_path, function(err) {
+                    res.sendfile(html_dir + '/viewTimeSheets.html');
+                    //res.send('File uploaded to: ' + target_path + ' - ' + req.files.timesheet.size + ' bytes');
+                });
+
             });
 
         });
-        
-    });
-}catch(e){
-        
-        timeSheet.findByIdAndUpdate(req.body.id, { $set: {  clientApproved: req.body.clientApproved,sunday: req.body.sunday,monday: req.body.monday,tuesday: req.body.tuesday,wednesday: req.body.wednesday,thursday: req.body.thursday,friday: req.body.friday,saturday: req.body.saturday }}, function (err, tank) {
-          res.sendfile(html_dir + '/viewTimeSheets.html');
+    } catch (e) {
 
-         
+        timeSheet.findByIdAndUpdate(req.body.id, {
+            $set: {
+                clientApproved: req.body.clientApproved,
+                sunday: req.body.sunday,
+                monday: req.body.monday,
+                tuesday: req.body.tuesday,
+                wednesday: req.body.wednesday,
+                thursday: req.body.thursday,
+                friday: req.body.friday,
+                saturday: req.body.saturday
+            }
+        }, function(err, tank) {
+            res.sendfile(html_dir + '/viewTimeSheets.html');
+
+
 
         });
 
-}
+    }
 
 
 
@@ -204,67 +278,94 @@ app.get('/editTimesheet', function(req, res) {
 });
 
 app.get('/showOptions', function(req, res) {
-    if(req.session.loggedInRole == 'admin')
-        data = [{name:"Add Employees", link: "createUser.html"},{name:"View Employees", link : "Users.html"}]
+    if (req.session.loggedInRole == 'admin')
+        data = [{
+            name: "Add Employees",
+            link: "createUser.html"
+        }, {
+            name: "View Employees",
+            link: "Users.html"
+        }]
     else
-        data = [{name:"Create TimeSheet", link: "createTimeSheet.html"},{name:"View TimeSheets", link : "viewTimeSheets.html"}]
+        data = [{
+            name: "Create TimeSheet",
+            link: "createTimeSheet.html"
+        }, {
+            name: "View TimeSheets",
+            link: "viewTimeSheets.html"
+        }]
     res.json(data);
 });
 
 app.get('/getLoggedInUser', function(req, res) {
-   
-    data = {user: req.session.loggedInUserData }
+
+    data = {
+        user: req.session.loggedInUserData
+    }
     res.json(data);
 });
 
 app.post('/loginUser', function(req, res) {
     console.log(req.body)
-    user.findOne({email : req.body.email, password : req.body.password}, function(err, data) {
-console.log(data)
-    if (data == null) {
-        //res.sendfile(html_dir + '/loginfailed.html');;
-        res.json({status:"failed"});
-    }
-    else{
-        req.session.loggedInUser = req.body.email;
-        req.session.loggedInRole = data.role
-        req.session.loggedInId = data._id
-        req.session.loggedInUserData = data
-        res.json({status:"success","user":data});
-       //res.sendfile(html_dir + '/userWelcome.html');
-    }
-        
+    user.findOne({
+        email: req.body.email,
+        password: req.body.password
+    }, function(err, data) {
+        console.log(data)
+        if (data == null) {
+            //res.sendfile(html_dir + '/loginfailed.html');;
+            res.json({
+                status: "failed"
+            });
+        } else {
+            req.session.loggedInUser = req.body.email;
+            req.session.loggedInRole = data.role
+            req.session.loggedInId = data._id
+            req.session.loggedInUserData = data
+            res.json({
+                status: "success",
+                "user": data
+            });
+            //res.sendfile(html_dir + '/userWelcome.html');
+        }
+
     });
-        
+
 
 });
 
 
 
 app.get('/showFile', function(req, res) {
-    FileContent.findOne({_id : req.query.id}, function(err, data) {
+    FileContent.findOne({
+        _id: req.query.id
+    }, function(err, data) {
 
-        res.setHeader('Content-disposition', 'attachment; filename='+req.query.originalName);
+        res.setHeader('Content-disposition', 'attachment; filename=' + req.query.originalName);
         //res.contentType('application/vnd.ms-excel');
         res.send(data.content);
-        
+
     });
-        
+
 
 });
 
 app.get('/removeFile', function(req, res) {
     FileContent.findByIdAndRemove(req.query.id, function(err, data) {
 
-        res.send({"status":"success"});
-        
+        res.send({
+            "status": "success"
+        });
+
     });
 
 });
 
 app.post('/addUser', function(req, res) {
-    user.findOne({email : req.body.email}, function(err, data) {
-        if(data == null){
+    user.findOne({
+        email: req.body.email
+    }, function(err, data) {
+        if (data == null) {
 
             var newUser = new user;
             console.log(req.body)
@@ -273,27 +374,33 @@ app.post('/addUser', function(req, res) {
             newUser.email = req.body.email;
             newUser.password = req.body.password;
             newUser.role = req.body.role;
-            newUser.save(function (err1) {
-                    if (err1) // ...
-                        res.send({"status":"failed"});
-           
-
+            newUser.save(function(err1) {
+                if (err1) // ...
+                    res.send({
+                    "status": "failed"
                 });
-             res.send({"status":"success"});
-        }else{
-          res.send({"status":"failed"});
+
+
+            });
+            res.send({
+                "status": "success"
+            });
+        } else {
+            res.send({
+                "status": "failed"
+            });
         }
     });
-    
+
     //res.sendfile(html_dir + '/Users.html');
 });
 
 app.post('/uploadFile', function(req, res) {
     fc = new FileContent
-    fs.readFile(req.files.upload.path, function(err, data){
+    fs.readFile(req.files.upload.path, function(err, data) {
         console.log(req.files.upload.path)
         fc.content = data;
-        fc.save(function (err,result) {
+        fc.save(function(err, result) {
             if (err) // ...
                 console.log(err);
 
@@ -301,7 +408,11 @@ app.post('/uploadFile', function(req, res) {
                 if (err) throw err;
                 //res.sendfile(html_dir + '/userWelcome.html');
                 console.log(result._id)
-                res.send({"status":"success","resultId":result._id,"fileName":req.files.upload.originalname});
+                res.send({
+                    "status": "success",
+                    "resultId": result._id,
+                    "fileName": req.files.upload.originalname
+                });
                 //res.send('File uploaded to: ' + target_path + ' - ' + req.files.timesheet.size + ' bytes');
             });
 
@@ -321,9 +432,11 @@ app.post('/createTimeSheet', function(req, res) {
     ts.uploadedFiles = req.body.uploadedFiles
     ts.sheets = req.body.sheets
     console.log(req.body.sheets)
-    ts.save(function (err) {
-        console.log("Timesheet Saved"+err)
-        res.send({"status":"success"});
+    ts.save(function(err) {
+        console.log("Timesheet Saved" + err)
+        res.send({
+            "status": "success"
+        });
     });
 
 
@@ -338,9 +451,21 @@ app.post('/modifyTimeSheet', function(req, res) {
     ts.uploadedFiles = req.body.uploadedFiles
     ts.sheets = req.body.sheets
     console.log(req.body.sheets)
-    timeSheet.findByIdAndUpdate(req.query.id,{ $set: {  sheets: req.body.sheets, comments: req.body.comments,uploadedFiles: req.body.uploadedFiles,startDate: req.body.startDate,endDate: req.body.endDate,clientApproved: req.body.clientApproved,total: req.body.total }}, function (err) {
-        console.log("Timesheet Updated"+err)
-        res.send({"status":"success"});
+    timeSheet.findByIdAndUpdate(req.query.id, {
+        $set: {
+            sheets: req.body.sheets,
+            comments: req.body.comments,
+            uploadedFiles: req.body.uploadedFiles,
+            startDate: req.body.startDate,
+            endDate: req.body.endDate,
+            clientApproved: req.body.clientApproved,
+            total: req.body.total
+        }
+    }, function(err) {
+        console.log("Timesheet Updated" + err)
+        res.send({
+            "status": "success"
+        });
     });
 
 
@@ -349,12 +474,12 @@ app.post('/addTimeSheet', function(req, res) {
     // get the temporary location of the file
     var tmp_path = req.files.timesheet.path;
     // set where the file should actually exists - in this case it is in the "images" directory
-   // var target_path = './uploads/timesheet_' + req.body.username+'.png';
+    // var target_path = './uploads/timesheet_' + req.body.username+'.png';
     // move the file from the temporary location to the intended location
 
-    
+
     var ts = new timeSheet;
-    fs.readFile(tmp_path, function(err, data){
+    fs.readFile(tmp_path, function(err, data) {
         ts.name = req.session.loggedInId;
         ts.startDate = req.body.startDate;
         ts.endDate = req.body.endDate;
@@ -370,7 +495,7 @@ app.post('/addTimeSheet', function(req, res) {
         ts.status = 0;
         ts.content = data;
         ts.originalName = req.files.timesheet.originalname;
-        ts.save(function (err) {
+        ts.save(function(err) {
             if (err) // ...
                 console.log(err);
 
@@ -385,6 +510,6 @@ app.post('/addTimeSheet', function(req, res) {
     });
 
 });
-var server = app.listen(8000, function () {
+var server = app.listen(8000, function() {
     console.log('listening on port %d', server.address().port);
 });
